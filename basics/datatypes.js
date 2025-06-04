a9cbe90230e83ee650bff566fefec2865568c9e0
Siddesh-G-H <siddeshghsiddesh@gmail.com>
2025-06-04 11:50:43 +0000

    Datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all JS code as new version
+
+// alert(3 + 3) // we are using node.js not browser
+
+console.log(3 
+    + 
+    3) // code readebility should be high
+console.log("Siddesh")
+
+let num = 53 // Number data-type => 2 to 2 power 53
+//bigint larger than 2 power 53
+let name = "Siddesh" // string
+let loggedIN = false // boolean
+//null => A Value in JS(Basically Standalone variable
+let state; // undefined (Value not assigned)
+//Symbol => For unique 
+
+console.log(typeof undefined)//Output = undefined
+console.log(typeof null) // output = object
