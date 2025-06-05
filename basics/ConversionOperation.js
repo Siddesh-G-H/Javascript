02f2cfa84e58519f6530f135bfbfdc6813bf483c
Siddesh-G-H <siddeshghsiddesh@gmail.com>
2025-06-05 15:12:18 +0000

    Adding About Operators

--- basics/ConversionOperation.js
+++ basics/ConversionOperation.js
@@ -9,11 +9,11 @@ let val = Number(num)
 // console.log(typeof(val));
 
 //console.log(val);// NaN is Not a Number is a value of class Number Therefore always check if a variable is not NaN
-let b = 33
-let res = String(b)
-console.log(res);
+// let b = 33
+// let res = String(b)
+// console.log(res);
 
-console.log(typeof res);
+// console.log(typeof res);
 
 /* Type Casting:
        To Number:
@@ -32,3 +32,42 @@ console.log(typeof res);
             true/false => "true"/"false" respectively
             33 => "33"
 */
+
+//***********************OPERATIONS***********************************************
+
+// let value = 3
+// let negval = -value
+// console.log(negval);
+
+// console.log(2+2);
+// console.log(2*2);
+// console.log(2-2);
+// console.log(2**4);
+// console.log(2/2);
+// console.log(2%2);
+
+// let str1 = "hello"
+// let str2 = " Siddesh"
+
+// let str3 = str1 + str2
+// console.log(str3);
+
+// let a = 1 + "2" // concatination
+// let b = "1" + 2 // concatination
+
+// console.log("1" + 2 + 2); // concatination from left to right
+
+// console.log(1 + 2 + "3"); // concatination from left to right but first add 1 and 2 therefore output is 33
+
+console.log(+true);
+
+console.log(+"");
+
+let a = b = c = 2
+console.log(a,b,c);
+
+b = a++ // value used before incrementing threfore b = 2
+c = ++a // value used after incrementing threfore c = 4
+
+console.log(a,b,c);
+
