ba05a7e32cdef8100b49e2ef2d17d141e026b6da
Siddesh-G-H <siddeshghsiddesh@gmail.com>
2025-06-10 17:11:47 +0000

    Added more about datatypes

--- basics/datatypes.js
+++ basics/datatypes.js
@@ -17,3 +17,24 @@ let state; // undefined (Value not assigned)
 
 console.log(typeof undefined)//Output = undefined
 console.log(typeof null) // output = object
+
+// primitive ,
+// 7 - types: String,Number,Boolean,null,undefined,Symbol,BigInt
+
+// Reference (Non-Primitive)
+// Arrays,Objects,Functions
+
+const arr = ['Shakthiman', 'Chota Bheem']
+
+const MyObj = {
+    name : "Siddesh G H",
+    age : 22,
+}
+
+const MyFunc = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof MyObj);
+console.log(typeof MyFunc);
+console.log(typeof arr);
