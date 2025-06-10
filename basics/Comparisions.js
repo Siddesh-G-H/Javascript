fb3c0947bd7fa0d7c2ae434d6f6a945e1a6858cf
Siddesh-G-H <siddeshghsiddesh@gmail.com>
2025-06-10 16:46:43 +0000

    About comparisions

---
+++ basics/Comparisions.js
@@ -0,0 +1,19 @@
+// console.log(2 > 3);
+// console.log(2 >= 3);
+// console.log(3 == 3);
+// console.log(2 != 3);
+
+// console.log("2" >= 1); // type converted to integer but typescript donot allow for this
+// console.log("02" >= 1);
+
+// console.log(null > 0); // null is diff and 0 is number therefore false
+// console.log(null == 0);
+// console.log(null >= 0); // but here >= convertes null to number 0 giving true as output
+
+// console.log(undefined >= 0);
+// console.log(undefined == 0);
+// console.log(undefined < 0);
+// console.log(undefined > 0);
+
+// === strict comparision with datatypes without conversion
+
